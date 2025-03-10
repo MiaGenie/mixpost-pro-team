65b88a59387fc74f257da0d1a33be0b77d977b2f
orion_pt <info@orioneurocorp.com>
2025-03-10 04:33:29 +0000

    Add GenieAdminMenu to SidebarAdmin

---
+++ resources/js/Components/Navigation/Genie/GenieAdminMenu.vue
@@ -0,0 +1,23 @@
+<script setup>
+import MenuItem from "@/Components/Sidebar/MenuItem.vue"
+import MenuDelimiter from "@/Components/Sidebar/MenuDelimiter.vue"
+import MenuGroupHeader from "@/Components/Sidebar/MenuGroupHeader.vue"
+import MenuGroupBody from "@/Components/Sidebar/MenuGroupBody.vue"
+import VersionIcon from "@/Icons/Genie/Version.vue";
+</script>
+<template>
+    <MenuGroupHeader>
+    {{ $t('genie.genie') }}
+    </MenuGroupHeader>
+    <MenuGroupBody>
+
+    <MenuItem :url="route('genie.admin.versions.index')"
+              :active="$page.component === 'Genie/Admin/Versions/Index'">
+        <template #icon>
+            <VersionIcon/>
+        </template>
+        {{ $t('genie.versions') }}
+    </MenuItem>
+    </MenuGroupBody>
+    <MenuDelimiter/>
+</template>
