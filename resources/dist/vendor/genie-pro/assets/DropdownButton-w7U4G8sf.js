95dd0a554eb7cc03baad3a7d32dcc7685f19484a
orion_pt <info@orioneurocorp.com>
2025-03-18 02:40:31 +0000

    Add Genie Files to AdminMenu

---
+++ resources/dist/vendor/genie-pro/assets/DropdownButton-w7U4G8sf.js
@@ -0,0 +1,1 @@
+import{g as a,o as t,w as s,a as o,I as e}from"./app-BLQQcgXx.js";import{E as r}from"./EllipsisVertical-BUUSwlBy.js";const m={__name:"DropdownButton",setup(c){return(_,n)=>(t(),a(e,{class:"mt-1"},{default:s(()=>[o(r)]),_:1}))}};export{m as _};
