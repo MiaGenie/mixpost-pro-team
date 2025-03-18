7e868bd8aaf1cfb3a122886fb84109508cfe1ee5
orion_pt <info@orioneurocorp.com>
2025-03-18 01:59:47 +0000

    Add Genie Config to AdminMenu and update genie translations

--- resources/js/Components/Navigation/Genie/GenieAdminMenu.vue
+++ resources/js/Components/Navigation/Genie/GenieAdminMenu.vue
@@ -4,20 +4,33 @@ import MenuDelimiter from "@/Components/Sidebar/MenuDelimiter.vue"
 import MenuGroupHeader from "@/Components/Sidebar/MenuGroupHeader.vue"
 import MenuGroupBody from "@/Components/Sidebar/MenuGroupBody.vue"
 import VersionIcon from "@/Icons/Genie/Version.vue";
+import CogIcon from "@/Icons/Cog.vue";
 </script>
 <template>
     <MenuGroupHeader>
     {{ $t('genie.genie') }}
     </MenuGroupHeader>
+
     <MenuGroupBody>
 
-    <MenuItem :url="route('genie.admin.versions.index')"
-              :active="$page.component === 'Genie/Admin/Versions/Index'">
-        <template #icon>
-            <VersionIcon/>
-        </template>
-        {{ $t('genie.versions') }}
-    </MenuItem>
+        <MenuItem :url="route('genie.admin.versions.index')"
+                  :active="$page.component === 'Genie/Admin/Versions/Index'">
+            <template #icon>
+                <VersionIcon/>
+            </template>
+            {{ $t('genie.versions') }}
+        </MenuItem>
+
+        <MenuDelimiter/>
+
+        <MenuItem :url="route('genie.admin.configs.form')"
+                  :active="$page.component === 'Genie/Admin/GenieAIConfig'">
+            <template #icon>
+                <CogIcon/>
+            </template>
+            {{ $t('genie.config') }}
+        </MenuItem>
+
     </MenuGroupBody>
     <MenuDelimiter/>
 </template>
